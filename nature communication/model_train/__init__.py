2232583edf824593e22cdf926a9a7614ad87abf3
Amos-Yooupi <96480065+Amos-Yooupi@users.noreply.github.com>
2025-07-30 11:18:16 +0800

    Delete nature communication directory

--- nature communication/model_train/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.dirname(__file__))
-from train import train
-from load_model import load_model
-from save_model import save_model
-from test_model import test
--- nature communication/utils/save_loss.py
+++
@@ -1,5 +0,0 @@
-def save_loss(loss_list, path):
-    with open(path, 'w') as f:
-        for loss in loss_list:
-            f.write(str(loss) + '\n')
-    print('Loss saved to', path)
